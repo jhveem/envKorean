040c40eeea83d48c5e6258db2c57ff1aa243b72b
Jhveem <joshua.hveem@gmail.com>
2018-10-18 14:34:57 -0600

    binder

---
+++ binder/custom.js
@@ -0,0 +1,17 @@
+function run() {
+	document.getElementById('run_all_cells').click();
+	alert('hi');
+}
+setTimeout(run, 1000);
+cells = document.getElementsByClassName('code_cell');
+/*
+for (var i = 0; i < cells.length - 1; i ++) {
+	//cells[i].style.visibility = "hidden";
+	cells[i].style.display='none';
+}
+//cells[cells.length-1].firstChild.style.display='none';
+function getLesson(me) {
+	alert('1');
+    var kernel = IPython.notebook.kernel;
+    kernel.execute('myStr = ' + me.value);
+}
